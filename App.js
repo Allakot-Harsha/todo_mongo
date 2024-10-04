18d9334a74e262b3a8ccf0e43277a71e2f09f782
Harsha <harshamohan023@gmail.com>
2024-10-04 13:17:28 -0400

    second commit

--- App.js
+++ App.js
@@ -2,13 +2,32 @@ const express = require("express");
 const app = express();
 const task=require("./routes/tasks")
 const port = 5000;
+const dotenv = require("dotenv");
 
+const connectDB = require("./DB/connect")
+dotenv.config();
+// require("dotenv").config();
+app.use(express.json());
 app.use("/api/v1/tasks",task);
+const url = process.env.MONGO_URL;
 
-app.listen(port,()=>{
-    console.log(`Server is listening at ${port}`);
-    
-})
+const start = async() =>{
+    try{
+        
+        await connectDB(url);
+        app.listen(port,()=>{
+            console.log(`Server is listening at ${port}`);
+            
+        })
+
+    }
+    catch(error){
+        console.log(error);
+        
+    }
+};
+
+start();
 //app.get('/api/v1/tasks')- get all the tasks
 //app.post('/api/v1/tasks')- create a new task
 // app.get('/api/v1/tasks/:id')- get a single task
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,11 +1,14 @@
 const router = require("../routes/tasks");
+const Tasks = require("../models/tasks")
 
 const getAllTask=(req,res)=>{
     res.send("get All Task");
 };
 
-const createTask=(req,res)=>{
-    res.send("create Task");
+const createTask=async (req,res)=>{
+    console.log(req.body);
+    const task = await Tasks.create(req.body);  
+    res.send(task);
 };
 
 const getTask =(req,res)=>{
